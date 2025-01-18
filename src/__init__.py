a1fe9ef2cd2e6435075c5c0566778ff1a47bffa9
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-17 21:09:06 -0600

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from .inspector import Inspector
 from .findings_extractor import extract_findings
 from .service_finder import get_service_findings, save_findings
-from .utils.aws_cli import run_aws_cli
+from utils.aws_cli import run_aws_cli
 
 __all__ = [
     'Inspector',
